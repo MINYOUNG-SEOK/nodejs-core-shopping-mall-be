da9104a2cf750af1e82a312f04fe98bdc8e91c0c
Rosie Seok <lovelyrosie731@gmail.com>
2025-08-03 00:31:15 +0900

    fix: CORS 설정 개선

--- app.js
+++ app.js
@@ -6,23 +6,12 @@ const indexRouter = require("./routes/index");
 const app = express();
 
 require("dotenv").config();
-
-// CORS 설정 개선
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://nodejs-core-shopping-mall-fe.netlify.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // req.body가 객체로 인식이 됨
 
 app.use("/api", indexRouter);
+// /api/user
 
 const mongoURI = process.env.MONGODB_URI_PROD;
 mongoose
