ea251eefe821a9b7a324c9eeb9a44b113f516701
Rosie Seok <lovelyrosie731@gmail.com>
2025-08-03 17:46:11 +0900

    feat: 상품 생성 API 구현 및 SKU 중복 체크 추가

---
+++ controllers/product.controller.js
@@ -0,0 +1,46 @@
+const Product = require("../models/Product");
+const productController = {};
+
+productController.createProduct = async (req, res) => {
+  try {
+    const {
+      sku,
+      name,
+      size,
+      image,
+      category,
+      description,
+      price,
+      stock,
+      status,
+    } = req.body;
+
+    // SKU 중복 체크
+    const existingProduct = await Product.findOne({ sku });
+    if (existingProduct) {
+      return res.status(400).json({
+        status: "실패",
+        error: `SKU ${sku}는 이미 존재합니다.`,
+      });
+    }
+
+    const product = new Product({
+      sku,
+      name,
+      size,
+      image,
+      category,
+      description,
+      price,
+      stock,
+      status,
+    });
+
+    await product.save();
+    res.status(200).json({ status: "성공", product });
+  } catch (error) {
+    res.status(400).json({ status: "실패", error: error.message });
+  }
+};
+
+module.exports = productController;
